cf637c3320c3feab1aa5af1c2eb4e2433487bcc5
Teo Basili <basili.teo@gmail.com>
2018-12-28 14:47:20 +0100

    NTP

--- NTP/user/user_main.c
+++ NTP/user/user_main.c
@@ -24,8 +24,6 @@
 #include "ets_sys.h"
 #include "osapi.h"
 #include "user_interface.h"
-#include "pwm.h"
-
 
 #if ((SPI_FLASH_SIZE_MAP == 0) || (SPI_FLASH_SIZE_MAP == 1))
 #error "The flash map is not supported"
@@ -63,16 +61,6 @@
 #error "The flash map is not supported"
 #endif
 
-
-os_timer_t timer_struct;
-uint32_t io_info[][3] = {
-    {PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4, 4 },
-};
-uint32_t duty[] = {0};
-os_timer_t timer_struct;
-uint32_t volatile dutyc = 0;
-volatile uint8_t direction = 1;
-
 static const partition_item_t at_partition_table[] = {
     { SYSTEM_PARTITION_BOOTLOADER, 						0x0, 												0x1000},
     { SYSTEM_PARTITION_OTA_1,   						0x1000, 											SYSTEM_PARTITION_OTA_SIZE},
@@ -90,24 +78,118 @@ void ICACHE_FLASH_ATTR user_pre_init(void)
 	}
 }
 
+#define HOSTNAME "ESP8266"
+#define SSID "CasaSu"
+#define PASSWORD "passwordgazzosa"
+
+struct station_config cfg;
+os_timer_t sntp_timer;
+
+void ICACHE_FLASH_ATTR
+print_mac(uint8 * mac){
+    uint8 i = 0;
+    for(i = 0; i < 6; i++){
+        os_printf("%02X" , mac[i]);
+        if(i != 5){
+            os_printf(":");
+        }
+    }
+    os_printf("\n");
+}
+
+
+void ICACHE_FLASH_ATTR
+callback(void){
+    uint32 current_stamp = 0;
+    current_stamp = sntp_get_current_timestamp();
+    if(current_stamp){
+        os_timer_disarm(&sntp_timer);
+        os_printf("sntp: %d, %s \n", current_stamp , sntp_get_real_time(current_stamp));
+        os_timer_arm(&sntp_timer, 10000, 0);
+    } else {
+        //os_printf("riarmo\n");
+        os_timer_arm(&sntp_timer, 500, 0);
+    }
+
+}
+
+
 void ICACHE_FLASH_ATTR
 system_init_cb(void)
 {
-    os_printf("Program started\n");
+    uart_init(115200,115200);
+    uint8 ret = 0;
+    ret = wifi_get_opmode_default();
+    os_printf("Operation mode = %d\n" , ret);
+    if(ret != STATION_MODE){
+        os_printf("Set operation mode to Station\n");
+        if(!wifi_set_opmode(STATION_MODE)){
+            os_printf("Error while set to Station\n");
+            return;
+        }        
+    }
+    os_printf("Connecting...\n");
+    if(wifi_station_set_hostname(HOSTNAME)){
+        os_printf("Hostname set to " HOSTNAME "\n");
+    } else {
+        os_printf("Unable to set hostname\n");
+    }
+
+    os_printf("Retriving wifi station configuration information\n");
+    ret = wifi_station_get_config_default(&cfg);
+
+    if(ret){
+        os_printf("ssid : %0.32s\npassword : %0.64s\nbssid : " MACSTR "\n", cfg.ssid, cfg.password, MAC2STR(cfg.bssid));        print_mac(cfg.bssid);
+    } else {
+        os_printf("error\n");
+    }
+
+    os_printf("Setting new wifi parameter\n");
+    os_memcpy(&cfg.ssid, SSID, 32);
+    os_memcpy(&cfg.password, PASSWORD, 64);
+    cfg.bssid_set = 0;
+    wifi_station_set_config(&cfg);
+
+    os_printf("Retriving wifi station configuration information\n");
+    ret = wifi_station_get_config_default(&cfg);
+
+    if(ret){
+        os_printf("ssid : %0.32s\npassword : %0.64s\nbssid : " MACSTR "\n", cfg.ssid, cfg.password, MAC2STR(cfg.bssid));
+    } else {
+        os_printf("error\n");
+    }
+
+    /* autoconnect enable by default*/
+    /*wifi_station_disconnect();
+    wifi_station_connect();*/
+    
+    sntp_setservername(0, "0.it.pool.ntp.org");
+    sntp_setservername(1, "1.it.pool.ntp.org");
+    sntp_setservername(2, "2.it.pool.ntp.org");
+    sntp_stop();
+    if(sntp_set_timezone(1)){
+        sntp_init();
+    } else {
+        os_printf("error timezone set");
+        return;
+    }
+
+    
+    os_timer_disarm(&sntp_timer);
+    os_timer_setfn(&sntp_timer, (os_timer_func_t *) callback, NULL);
+    os_timer_arm(&sntp_timer, 100 ,0);
+
 }
 
-void ICACHE_FLASH_ATTR
-callback(void *args){
-    os_printf("Analog read: %d\n", system_adc_read());
-}
 
-
+/**
+ * @brief Test spi interfaces.
+ *
+ */
 void ICACHE_FLASH_ATTR
 user_init(void)
 {
-    uart_init(115200,115200);
-    os_timer_disarm(&timer_struct);
-    os_timer_setfn(&timer_struct, callback, NULL);
-    os_timer_arm(&timer_struct, 50, true);
+    
     system_init_done_cb(system_init_cb);
 }
+
